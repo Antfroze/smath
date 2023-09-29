5ab3c509f6bbbdb98db559b46a2efe60a7954f31
Antfroze <realantfroze@gmail.com>
2023-09-29 18:29:01 -0500

    Add *= to rectangle

--- include/rect.hpp
+++ include/rect.hpp
@@ -13,10 +13,6 @@ struct Rectangle {
     inline const T GetArea() { return GetWidth() * GetHeight(); }
     inline const Vector2<T> Center() { return origin + size / 2; }
 
-    inline Rectangle operator*(const T& num) const {
-        return Rectangle(origin * num, size * num);
-    }
-
     inline bool Contains(const Vector2<T>& position) {
         return (position.x >= origin.x && position.x <= (origin.x + size.x) &&
                 position.y >= origin.y && position.y <= (origin.y + size.y));
@@ -26,6 +22,15 @@ struct Rectangle {
         return (other.origin.x >= origin.x && other.origin.y >= origin.y &&
                 (other.origin.x + other.size.x) <= (origin.x + size.x) &&
                 (other.origin.y + other.size.y) <= (origin.y + size.y));
+    }
+
+    inline Rectangle operator*(const T& num) const {
+        return Rectangle(origin * num, size * num);
+    }
+    inline Rectangle& operator*=(const T& num) {
+        origin *= num;
+        size *= num;
+        return *this;
     }
 
     Vector2<T> origin, size;
